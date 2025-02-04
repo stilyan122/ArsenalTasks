ce20fc8e6c13bafc69eb090c279df93a201d53e3
stilyan122 <stilancanev@gmail.com>
2025-02-04 11:38:41 +0200

    Commit files

---
+++ 04.02.2025/04.02.2025/04.02.2025/Player.cs
@@ -0,0 +1,42 @@
+﻿namespace _04._02._2025
+{
+    public class Player
+    {
+		private string position = string.Empty;
+		private string name = string.Empty;
+
+        public Player(string position, string name)
+        {
+            this.Position = position;
+            this.Name = name;
+        }
+
+        public string Position
+		{
+			get { return position; }
+			set 
+			{
+				if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("Player position is invalid!");
+				}
+
+				position = value; 
+			}
+		}
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Player name is invalid!");
+                }
+
+                name = value;
+            }
+        }
+    }
+}
