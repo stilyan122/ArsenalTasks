a42410dcb16f23a0784c706bc456a2343b19d963
stilyan122 <stilancanev@gmail.com>
2025-11-12 15:15:14 +0200

    Finished Internet Programming Project 1

---
+++ 12.11.2025/Ex1/MoviesWebApp/MoviesData/MoviesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesData.Entities;
+
+namespace MoviesData
+{
+    public class MoviesDBContext : DbContext
+    {
+        public MoviesDBContext(DbContextOptions<MoviesDBContext> options)
+            : base(options)
+        {  
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ 12.11.2025/Ex1/MoviesWebApp/MoviesWebApp/Controllers/MoviesController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesData;
+using MoviesData.Entities;
+
+namespace MoviesWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesDBContext context;
+
+        public MoviesController(MoviesDBContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.ToList();
+            return View(movies);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            context.Movies.Add(movie);
+            context.SaveChanges();
+            return RedirectToAction("Create");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var movie = context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var movie = context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            context.Movies.Remove(movie);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var movie = context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            context.Movies.Update(movie);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
